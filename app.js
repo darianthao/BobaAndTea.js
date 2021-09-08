2c389184ee8e6cabed0d33a063b061b827d9ad97
unknown <justdarian01@gmail.com>
2021-09-08 15:49:52 -0500

    added scroll wheel effects & hideMobileNavBar feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const hamburgerMenu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
 
 // Display Hamburger Menu
 const mobileMenu = () => {
@@ -8,3 +9,58 @@ menuLinks.classList.toggle('active')
 }
 
 hamburgerMenu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homePage= document.querySelector('#home-page');
+    const aboutPage = document.querySelector('#about-page');
+    const menuPage = document.querySelector('#menu-page');
+    const locationPage = document.querySelector('#location-page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
+  
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+      homePage.classList.add('highlight');
+      aboutPage.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+      aboutPage.classList.add('highlight');
+      homePage.classList.remove('highlight');
+      menuPage.classList.remove('highlight');
+      locationPage.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 2500) {
+      menuPage.classList.add('highlight');
+      locationPage.classList.remove('highlight');
+      aboutPage.classList.remove('highlight');
+      homePage.classList.remove('highlight');
+      return;
+    }
+    else if (window.innerWidth > 960 && scrollPos < 3700) {
+        locationPage.classList.add('highlight');
+        menuPage.classList.remove('highlight');
+        return;
+      }
+  
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+      elem.classList.remove('highlight');
+    }
+  };
+
+window.addEventListener('scroll', highlightMenu);
+
+window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars)
+        {
+            hamburgerMenu.classList.toggle('is-active')
+            menuLinks.classList.remove('active')
+        }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
